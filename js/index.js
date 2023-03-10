21257fe4df17ec0de3bbee3dfbfa64eb522c787c
Stan Andrei <vladut.stan@student.uaic.ro>
2023-03-10 17:53:44 +0200

    Update v0.1.1 - Animated new post button

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+let npbtData;
+
+function toggleNewPostAnimations() {
+    
+    var npb = document.getElementById("newPostButton");
+    var npbt = document.getElementById('newPostButtonText');
+    var nfc = document.getElementById("hiddenFormContainer");
+
+    if (npb.classList.contains("newPostButtonClicked")) {
+        npb.classList.remove("newPostButtonClicked");
+        npbt.innerHTML = npbtData;
+    } else {
+        npb.classList.add("newPostButtonClicked");
+        npbtData = npbt.textContent;
+        npbt.innerHTML = 'Dont click';
+    }
+
+    if (nfc.classList.contains("hidden")) {
+        nfc.classList.remove("hidden");
+    } else {
+        nfc.classList.add("hidden");
+    }  
+
+}
