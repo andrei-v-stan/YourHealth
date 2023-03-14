0bfbceee3275fd8f8587dc70d3bb4fe56e2103c4
Stan Andrei <vladut.stan@student.uaic.ro>
2023-03-14 18:04:15 +0200

    v0.1.5 - Post creation and display

--- js/node.js
+++ js/node.js
@@ -58,6 +58,60 @@ transporter.sendMail(mailOptions, (error, info) => {
 });
 
 
+app.post('/create_post', (req, res) => {
+  const { titlePost, contentPost, authorPost } = req.body;
+
+  con.query('INSERT INTO posts (title, content, author, created_at) VALUES (?, ?, ?, NOW())', [titlePost, contentPost, authorPost], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error creating post');
+    } else {
+      res.redirect('/posts');
+    }
+  });
+});
+
+app.get('/posts', (req, res) => {
+  con.query('SELECT * FROM posts ORDER BY created_at DESC', (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.send(`
+      <html>
+        <body>
+          <h1>Page</h1>
+          <ul>
+            ${results.map(post => `<li><a href="/posts/${post.id}">${post.title}</a></li>`).join('')}
+          </ul>
+          <a href="/posts/new">New Post</a>
+        </body>
+      </html>
+    `);
+  });
+});
+
+app.get('/posts/:id', (req, res) => {
+  const postId = req.params.id;
+  con.query('SELECT * FROM posts WHERE id = ?', [postId], (err, results) => {
+    if (err) {
+      throw err;
+    }
+    const post = results[0];
+    res.send(`
+      <html>
+        <head>
+          <title>${post.title}</title>
+        </head>
+        <body>
+          <h1>${post.author}</h1>
+          <p>${post.content}</p>
+        </body>
+        </html>
+      `);
+    });
+  });
+
+
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${3000}`);
