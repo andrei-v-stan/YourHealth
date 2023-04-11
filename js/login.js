7639381309cc0c6af11b8becc9933fed5a111871
Stan Andrei <vladut.stan@student.uaic.ro>
2023-04-11 21:45:39 +0300

    v0.1.13.1 - Added other popups and base design

--- js/login.js
+++ js/login.js
@@ -1,21 +1,103 @@
-const loginButton = document.getElementById('loginButton');
-const loginBackground = document.getElementById('loginBackground');
-const loginPopupTop = document.getElementById('loginPopupTop');
-const loginPopup = document.getElementById('loginPopup');
-const loginPopupBottom = document.getElementById('loginPopupBottom');
 
-loginButton.addEventListener('click', () => {
-    loginBackground.style.display = 'block';
-    loginPopupTop.style.display = 'block';
-    loginPopup.style.display = 'block';
-    loginPopupBottom.style.display = 'block';
-	document.body.style.overflow = 'hidden';
-});
+const loginBack = document.getElementById('loginBackground');
+const loginPopTop = document.getElementById('loginPopupTop');
+const loginPop = document.getElementById('loginPopup');
 
-loginBackground.addEventListener('click', () => {
-    loginBackground.style.display = 'none';
-    loginPopupTop.style.display = 'none';
-	loginPopup.style.display = 'none';
-    loginPopupBottom.style.display = 'none';
+const signupBack = document.getElementById('signupBackground');
+const signupPopTop = document.getElementById('signupPopupTop');
+const signupPop = document.getElementById('signupPopup');
+
+const recoveryBack = document.getElementById('recoveryBackground');
+const recoveryPopT = document.getElementById('recoveryPopupTop');
+const recoveryMenuPop = document.getElementById('recoveryMenuPopup');
+const recoverPassPop = document.getElementById('recoverPasswordPopup');
+const contactFormPop = document.getElementById('contactFormPopup');
+
+
+loginBack.addEventListener('click', () => {
+    removeLoginPopup();
 	document.body.style.overflow = 'auto';
 });
+
+signupBack.addEventListener('click', () => {
+    removeSignupPopup();
+    loginPopup();
+	document.body.style.overflow = 'auto';
+});
+
+recoveryBack.addEventListener('click', () => {
+    removeRecoveryPopup();
+	loginPopup();
+    document.body.style.overflow = 'auto';
+});
+
+
+function removeLoginPopup() {
+    loginBack.style.display = 'none';
+    loginPopTop.style.display = 'none';
+    loginPop.style.display = 'none';
+}
+
+function removeSignupPopup() {
+    signupBack.style.display = 'none';
+    signupPopTop.style.display = 'none';
+    signupPop.style.display = 'none';
+}
+
+function removeRecoveryPopup() {
+    recoveryBack.style.display = 'none';
+    recoveryPopT.style.display = 'none';
+    recoveryMenuPop.style.display = 'none';
+    recoverPassPop.style.display = 'none';
+    contactFormPop.style.display = 'none';
+}
+
+
+function loginPopup() {
+    removeRecoveryPopup();
+    removeSignupPopup();
+    loginBack.style.display = 'block';
+    loginPopTop.style.display = 'block';
+    loginPop.style.display = 'block';
+	document.body.style.overflow = 'hidden';
+}
+
+function signupPopup() {
+    removeLoginPopup();
+    signupBack.style.display = 'block';
+    signupPopTop.style.display = 'block';
+    signupPop.style.display = 'block';
+	document.body.style.overflow = 'hidden';
+}
+
+function recoveryMenuPopup() {
+    recoverPassPop.style.display = 'none';
+    contactFormPop.style.display = 'none';
+    removeLoginPopup();
+    recoveryBack.style.display = 'block';
+    recoveryPopT.style.display = 'block';
+    recoveryMenuPop.style.display = 'block';
+	document.body.style.overflow = 'hidden';
+}
+
+function recoverPasswordPopup() {
+    recoveryMenuPop.style.display = 'none';
+    recoverPassPop.style.display = 'block';
+	document.body.style.overflow = 'hidden';
+}
+
+function contactFormPopup() {
+    recoveryMenuPop.style.display = 'none';
+    contactFormPop.style.display = 'block';
+	document.body.style.overflow = 'hidden';
+}
+
+
+
+function test(formX) {
+    const form = document.getElementById(formX);
+    const formData = new FormData(form);
+    for (const value of formData.values()) {
+        console.log(value);
+  }
+}
